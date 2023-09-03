ae0d9e3b3f6d60aee7dc4ee30bc7283609b58c32
Sachin <skpkorba9009@gmail.com>
2023-09-03 19:01:20 +0530

    all done

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const PORT = process.env.PORT || 5001;
 const MONGO_URL = process.env.MONGO_URI || 'mongodb://localhost:27017/todo';
-const JWT_SECRET = process.env.JWT_SECRET || Sample123;
+const JWT_SECRET = process.env.JWT_SECRET || 'Sample123';
 module.exports = {
     PORT,
     MONGO_URL,
